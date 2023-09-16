9508b5317d5831fd513f8bc44ed06a25db288ffc
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-16 14:52:51 -0400

    create initial login page proof of concept

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,35 @@
+import { useRouter } from "next/router";
+import { useRef, useState } from "react";
+
 export default function Login(){
-  return (<>login page</>);
+  const router = useRouter();
+  const usernameInput = useRef();
+  const passwordIndput = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const username = usernameInput.current.value;
+    const password = passwordIndput.current.value;
+  
+    const res = await fetch("/api/sessions", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({username, password})
+    });
+
+    if(res.ok){
+      // re-route to proper page
+      //return router.push("/page");
+    }
+  }
+  return (
+  <form onSubmit={handleSubmit}>
+    <label htmlFor="username">username: </label>
+    <input type="text" id="username" ref={usernameInput} />
+    <label htmlFor="password">password: </label>
+    <input type="password" id="password" ref={passwordIndput} />
+    <button type="submit">Sign In</button>
+  </form>
+  );
 }
