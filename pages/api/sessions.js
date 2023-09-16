08187afdf0517dc69887cffb4644e4c41f28f5bd
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-16 12:27:40 -0400

    create test API to  auth users

---
+++ pages/api/sessions.js
@@ -0,0 +1,25 @@
+import {withSessionRoute} from "pages/lib/config/withSession";
+
+const TEST_USERNAME = "testuser";
+const TEST_PASSWORD = "password";
+
+export default withSessionRoute(createSessionRoute);
+
+async function createSessionRoute(req, res){
+  if(req.method === "POST"){
+    const {user, password} = req.body;
+
+    if(user === TEST_USERNAME && password === TEST_PASSWORD){
+      req.session.user = {
+        username: user,
+        permisions: 0,
+      };
+      await req.session.save();
+      res.send({ok:true});
+    }
+
+    return res.status(403).send("");
+  }
+  
+  return res.status(404).send("");
+}
