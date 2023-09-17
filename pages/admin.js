a5c9383bff4dcaaa4bc0e0210cbf9ec5dea97723
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-17 11:49:47 -0400

    add user create form

--- pages/admin.js
+++ pages/admin.js
@@ -1,4 +1,5 @@
 import { withSessionSsr  } from "./lib/config/withSession";
+import { useRef, useState } from "react";
 
 export const getServerSideProps = withSessionSsr(
   async ({req, res}) => {
@@ -19,12 +20,78 @@ export const getServerSideProps = withSessionSsr(
 )
 
 export default function Admin(props){
+  const usernameInput = useRef();
+  const passwordInput = useRef();
+  const permissionsInput = useRef();
+  const firstnameInput = useRef();
+  const lastnameInput = useRef();
+  const gradyearInput = useRef();
+
+  async function handleCreateUserSubmit(e){
+    e.preventDefault();
+
+    const res = await fetch("/api/createuser", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        username: usernameInput.current.value,
+        password: passwordInput.current.value,
+        permisions: permissionsInput.current.value,
+        userinfo: {
+          firstname: firstnameInput.current.value,
+          lastname: lastnameInput.current.value,
+          gradyear: gradyearInput.current.value
+        }
+      })
+    })
+
+    if(res.ok){
+      usernameInput.current.value = "";
+      passwordInput.current.value = "";
+      permissionsInput.current.value = "";
+      firstnameInput.current.value = "";
+      lastnameInput.current.value = "";
+      gradyearInput.current.value = "";
+      alert("created user sucessfully");
+    } else {
+      alert("failed to create user");
+    }
+  }
+
   return (
     <>
       <h1>Admin Page!</h1>
-      <div> logged in as <b>{props.user.username}</b></div>
+      <div>logged in as <b>{props.user.username}</b></div>
       <div>
         <a href="/">home</a>
+      </div>
+      <div>
+        <form onSubmit={handleCreateUserSubmit}>
+          <h2>create user</h2>
+          <div>
+            <label htmlFor="username">username: </label>
+            <input type="text" id="username" ref={usernameInput} />
+          </div>
+          <div>
+            <label htmlFor="password">password: </label>
+            <input type="text" id="password" ref={passwordInput} />
+          </div>
+          <div>
+            <label htmlFor="permissions">permission level: (0 basic, 1 admin): </label>
+            <input type="number" min="0" max="1" step="1" id="permissions" ref={permissionsInput} />
+          </div>
+          <div>
+            <label htmlFor="firstname">firstname: </label>
+            <input type="text"  id="firstname" ref={firstnameInput} />
+            <label htmlFor="lastname"> lastname: </label>
+            <input type="text"  id="lastname" ref={lastnameInput} />
+          </div>
+          <div>
+            <label htmlFor="gradyear">gradyear: </label>
+            <input type="number" min="1900" max="2200" step="1" id="gradyear" ref={gradyearInput} />
+          </div>
+          <button type="submit">Create User</button>
+        </form>
       </div>
     </>
   );
