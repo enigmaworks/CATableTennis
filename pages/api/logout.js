6e5adef1b94ff851d5bd492e56ec49f0d611d1f5
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-19 09:19:02 -0400

    remove unused function param

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -2,7 +2,7 @@ import { withSessionRoute } from "pages/lib/config/withSession";
 
 export default withSessionRoute(logout);
 
-async function logout(req, res, session) {
+async function logout(req, res) {
   req.session.destroy();
   res.send({ok:true});
 }
