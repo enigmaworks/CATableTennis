d012d490face6c488bd666e2bd95c871da50f316
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-21 09:03:07 -0400

    stricter session validation

--- pages/api/users/create.js
+++ pages/api/users/create.js
@@ -8,7 +8,7 @@ function create(req, res){
     const {username, password, permissions, userinfo} = req.body;
     if(users.findUser(username)) return res.status(400).send();
 
-    if(req.session) {
+    if(req.session && req.session.user) {
       if(req.session.user.permissions === 1){
         createUser(username, password, permissions, userinfo);
       } else {
