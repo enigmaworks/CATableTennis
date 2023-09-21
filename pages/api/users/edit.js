a8b3085ccf57c8aef326b283171f445904802334
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-21 08:42:09 -0400

    create protected API route to edit users

---
+++ pages/api/users/edit.js
@@ -0,0 +1,23 @@
+import {withSessionRoute} from "lib/config/withSession";
+import users from "helpers/users-data";
+
+export default withSessionRoute(getusersdata);
+
+function getusersdata(req, res){
+  if(req.method = "POST"){
+    const {id, data} = req.body;
+
+    if(req.session && req.session.user){
+      try{
+        users.updateUser(id, data);
+        return res.status(200).send();
+      } catch {
+        return res.status(500).send();
+      }
+    } else {
+      return res.status(401).send();
+    }
+  } else {
+    return res.status(405).send();
+  }
+}
