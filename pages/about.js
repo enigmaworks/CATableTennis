b376f2797f1243117babfc914a0cacb316cf9e9d
bob <bob362018@outlook.com>
2023-09-21 12:30:11 -0400

    test fonts, add heading

--- pages/about.js
+++ pages/about.js
@@ -2,8 +2,13 @@ import "./styles/about.css";
 
 export default function about() {
   return (
-    <div className="image">
-      <img className="CATT-logo" alt="Catt logo" src="/CATTLogo.png" />
-    </div>
+    <>
+      <div className="image">
+        <img className="CATT-logo" alt="Catt logo" src="/CATTLogo.png" />
+      </div>
+      <div className="label">
+        <div className="text-wrapper">About CATT</div>
+      </div>
+    </>
   );
 };
