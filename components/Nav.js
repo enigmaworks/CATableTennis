069a1fbbc74c7b9b0165903623b24786d0e33d54
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-22 13:32:11 -0400

    add style for active link

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,11 @@
 import { useRef, useState } from "react";
+import { useRouter } from 'next/router'
+
 import styles from "styles/components/nav.module.css"
 
 export default function Nav(){
   const [navIsOpen, setNavIsOpen] = useState(false);
+  const router = useRouter();
 
   function handleOnClick(){
     setNavIsOpen(!navIsOpen);
@@ -12,12 +15,12 @@ export default function Nav(){
     <nav className={styles.nav} nav-state={navIsOpen ? "open" : "closed"}>
       <div className={styles.navcontainer}>
       <button className={styles.navbutton} onClick={handleOnClick}>{navIsOpen ? "Close" : "Menu"}</button>
-        <ul className={styles.navlist} aria-hidden={!navIsOpen} >
-          <li className="button"><a href="/">Home</a></li>
-          <li className="button"><a href="/match">Match</a></li>
-          <li className="button"><a href="/players">Players</a></li>
-          <li className="button"><a href="/about">About</a></li>
-        </ul>
+      <ul className={styles.navlist} aria-hidden={!navIsOpen} >
+        <li className="button" is-active={router.pathname === "/" ? "true" : "false"}><a href="/">Home</a></li>
+        <li className="button" is-active={router.pathname === "/match" ? "true" : "false"}><a href="/match">Match</a></li>
+        <li className="button" is-active={router.pathname === "/players" ? "true" : "false"}><a href="/players">Players</a></li>
+        <li className="button" is-active={router.pathname === "/about" ? "true" : "false"}><a href="/about">About</a></li>
+      </ul>
       </div>
     </nav>
   )
