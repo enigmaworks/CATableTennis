a652f4b12ec5303a2dcd44533be346354f7052d7
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-21 20:29:56 -0400

    create basic nav component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import "/styles/globals.css";
+import Nav from "../components/Nav";
 
 export default function App({ Component, pageProps }) {
   return( <>
+    <Nav></Nav>
     <main>
       <Component {...pageProps} />
     </main>
