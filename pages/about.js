4c74c763551c48a8d70878ef4ab3576432bf2ca9
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-21 21:14:35 -0400

    update styles and add global props

--- pages/about.js
+++ pages/about.js
@@ -1,28 +1,31 @@
-// import "/styles/about.css";
+import styles from "styles/about.module.css";
 
 export default function about() {
   return (
     <>
  
       
-      <div class="heading">
-        <img class="CATT-logo" alt="CATT logo" src="/CATTLogo.png" />
+      <header className={styles.heading}>
+        <img className={styles.logo} alt="Catt logo" src="/CATTLogo.png" />
         <h1>About CATT</h1>
-      </div>
-      <div class="maintext">
+      </header>
+      <section>
+        <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
         dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
         ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
         nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
         anim id est laborum.
-        
+        </p>
+        <p>
         Vitae tortor condimentum lacinia quis vel eros donec ac odio. Egestas egestas fringilla phasellus faucibus
         scelerisque eleifend donec. Ridiculus mus mauris vitae ultricies. Iaculis urna id volutpat lacus laoreet non
         curabitur gravida arcu. Eu ultrices vitae auctor eu augue ut lectus. Erat nam at lectus urna duis. Aenean
         sed adipiscing diam donec. Fermentum leo vel orci porta non pulvinar neque. Fermentum et sollicitudin ac
         orci phasellus egestas tellus. Quam id leo in vitae turpis massa sed elementum. Vitae aliquet nec
         ullamcorper sit amet risus nullam eget felis.
-      </div>
+        </p>
+      </section>
     </>
   );
 };
