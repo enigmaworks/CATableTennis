2aa29eff6f0bbec9f5a339b8d294ec1b5fe9503c
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-25 08:58:23 -0400

    create NavLink component

--- components/Nav.js
+++ components/Nav.js
@@ -3,26 +3,34 @@ import { useRouter } from 'next/router'
 
 import styles from "styles/components/nav.module.css"
 
-export default function Nav(){
+export default function Nav(pageprops){
   const [navIsOpen, setNavIsOpen] = useState(false);
-  const router = useRouter();
-
+  
   function handleOnClick(){
     setNavIsOpen(!navIsOpen);
   }
-
+  
   return (
     <nav className={styles.nav} nav-state={navIsOpen ? "open" : "closed"}>
       <div className={styles.navcontainer}>
       <button className={styles.navbutton} onClick={handleOnClick}>{navIsOpen ? "Close" : "Menu"}</button>
       <ul className={styles.navlist} aria-hidden={!navIsOpen} >
-        <li className="button" is-active={router.pathname === "/" ? "true" : "false"}><a href="/">Home</a></li>
-        <li className="button" is-active={router.pathname === "/players" ? "true" : "false"}><a href="/players">Players</a></li>
-        <li className="button" is-active={router.pathname === "/match" ? "true" : "false"}><a href="/match">Match</a></li>
-        <li className="button" is-active={router.pathname === "/calendar" ? "true" : "false"}><a href="/calendar">Calendar</a></li>
-        <li className="button" is-active={router.pathname === "/about" ? "true" : "false"}><a href="/about">About</a></li>
+        <NavLink linkhref="/">Home</NavLink>
+        <NavLink linkhref="/players">Players</NavLink>
+        <NavLink linkhref="/match">Match</NavLink>
+        <NavLink linkhref="/calendar">Calendar</NavLink>
+        <NavLink linkhref="/about">About</NavLink>
       </ul>
       </div>
     </nav>
   )
+}
+
+function NavLink({linkhref, children}){
+  const router = useRouter();
+  return (
+    <li className="button" is-active={router.pathname === linkhref ? "true" : "false"}>
+      <a href={linkhref}>{children}</a>
+    </li>
+  );
 }
