321db538e0a2cc6072efb834f0eecf1b23347481
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-27 13:19:10 -0400

    remove user auth for

--- pages/api/users/getdata.js
+++ pages/api/users/getdata.js
@@ -1,26 +1,19 @@
-import {withSessionRoute} from "helpers/lib/config/withSession";
 import users from "helpers/users-data";
 
-export default withSessionRoute(getusersdata);
-
-function getusersdata(req, res){
+export default function getusersdata(req, res){
   if(req.method = "GET"){
-    if(req.session && req.session.user){
-      let usersdata = users.getAll();
-      usersdata = usersdata.map(user => {
-        return {
-          id: user.id,
-          lastStatUpdate: user.lastStatUpdate,
-          username: user.username,
-          permissions: user.permissions,
-          info: user.info,
-          statistics: user.statistics,
-        }
-      })
-      return res.json(usersdata);
-    } else {
-      return res.status(401).send([]);
-    }
+    let usersdata = users.getAll();
+    usersdata = usersdata.map(user => {
+      return {
+        id: user.id,
+        lastStatUpdate: user.lastStatUpdate,
+        username: user.username,
+        permissions: user.permissions,
+        info: user.info,
+        statistics: user.statistics,
+      }
+    })
+    return res.json(usersdata);
   } else {
     return res.status(405).send([]);
   }
