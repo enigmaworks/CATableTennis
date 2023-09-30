f144608333fbb153e6aa0f771ed72a8092cafc1c
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-09-30 17:15:49 -0400

    set up react select

---
+++ helpers/select-theme.js
@@ -0,0 +1,10 @@
+export default function (basetheme) {
+  return {
+      ...basetheme,
+      colors: {
+        ...basetheme.colors,
+        primary: '#691616',
+        primary25: '#40404040',
+      }
+  }
+}
