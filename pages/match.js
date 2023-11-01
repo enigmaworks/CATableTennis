92cac7747c7c2a8b4a7fb2aadeaf855724fff27f
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-11-01 10:27:18 -0400

    add redability and default values

--- pages/match.js
+++ pages/match.js
@@ -9,7 +9,7 @@ export const getServerSideProps = withSessionSsr(
     if(user && user.permissions === 1){
       let data = await fetch(process.env.URL + "/api/users/getdata", req);
       data = await data.json();
-      return {props: { signedin: true, user: user, usersdata: data }}
+      return {props: { signedin: true, user: user, usersdata: data}}
     } else {
       return {
         redirect: {
@@ -24,8 +24,8 @@ export const getServerSideProps = withSessionSsr(
 export default function MatchPage(props){
   const [gamePhase, setgamePhase] = useState("setup");
   const [numPlayers, setNumPlayers] = useState(2);
-  const [team1, setTeam1] = useState([]);
-  const [team2, setTeam2] = useState([]);
+  const [team1, setTeam1] = useState([props.usersdata[0].id]);
+  const [team2, setTeam2] = useState([props.usersdata[1].id]);
   const matchTypeSelect = useRef();
   const p1Select = useRef();
   const p2Select = useRef();
@@ -33,9 +33,18 @@ export default function MatchPage(props){
   const p4Select = useRef();
 
   function playerIsAvailable(id){
-    for(let i = 0; i < props.usersdata.length; i++){
-      if(team1.includes(id) || team2.includes(id)) return false;
-      return true;
+    if(team1.includes(id) || team2.includes(id)) return true;
+    return false;
+  }
+  function handleNumPlayersChange(){
+    let num = parseInt(matchTypeSelect.current.value)
+    setNumPlayers(num);
+    if(num === 2){
+      setTeam1([team1[0]])
+      setTeam2([team2[0]])
+    } else {
+      setTeam1([props.usersdata[0].id, props.usersdata[2].id])
+      setTeam2([props.usersdata[1].id, props.usersdata[3].id])
     }
   }
   function updateTeam(num){
@@ -66,23 +75,59 @@ export default function MatchPage(props){
     </header>
     <section>
       <h2>Setup</h2>
-      <select ref={matchTypeSelect} onChange={()=>{setNumPlayers(parseInt(matchTypeSelect.current.value))}}>
+      <select
+        ref={matchTypeSelect}
+        onChange={handleNumPlayersChange}
+      >
         <option value="2">Two Player</option>
         <option value="4">Four Player</option>
       </select>
       <div>
         <h3>{numPlayers === 2 ? "Player One" : "Team One"}</h3>
-        <UserSelect usersdata={props.usersdata} refobj={p1Select} checkfn={playerIsAvailable} changefn={()=>{updateTeam(1)}}></UserSelect>
-        {numPlayers === 4 ? <UserSelect usersdata={props.usersdata} refobj={p2Select} checkfn={playerIsAvailable} changefn={()=>{updateTeam(1)}}></UserSelect> : ""}
+        <UserSelect
+          defaultvalue={team1[0]}
+          usersdata={props.usersdata}
+          refobj={p1Select}
+          checkfn={playerIsAvailable}
+          changefn={ ()=>{updateTeam(1)} }
+        />
+        { (numPlayers === 4) ?
+          <UserSelect
+            defaultvalue={team1[1]}
+            usersdata={props.usersdata}
+            refobj={p2Select}
+            checkfn={playerIsAvailable} 
+            changefn={()=>{updateTeam(1)}}
+          /> 
+        : ""}
+
       </div>
 
       <div>
         <h3>{numPlayers === 2 ? "Player Two" : "Team Two"}</h3>
-        <UserSelect usersdata={props.usersdata} refobj={p3Select} checkfn={playerIsAvailable} changefn={()=>{updateTeam(2)}}></UserSelect>
-        {numPlayers === 4 ? <UserSelect usersdata={props.usersdata} refobj={p4Select} checkfn={playerIsAvailable} changefn={()=>{updateTeam(2)}}></UserSelect> : ""}
-      </div>
+        <UserSelect
+          defaultvalue={team2[0]}
+          usersdata={props.usersdata}
+          refobj={p3Select}
+          checkfn={playerIsAvailable}
+          changefn={ ()=>{updateTeam(2)} }
+        />
+        { (numPlayers === 4) ?
+          <UserSelect
+            defaultvalue={team2[1]}
+            usersdata={props.usersdata}
+            refobj={p4Select}
+            checkfn={playerIsAvailable} 
+            changefn={()=>{updateTeam(2)}}
+          /> 
+        : ""}
+       </div>
       
-      <button onClick={()=>{setgamePhase("match")}}>Next</button>
+      <button onClick={()=>{
+        if(team1.length + team2.length === numPlayers){
+          setgamePhase("match")
+        }
+        }}> Next </button>
     </section>
   </>);
   }
@@ -99,17 +144,26 @@ function Match({numPlayers, teamone, teamtwo, backfn}){
   }
 }
 
-function UserSelect({usersdata, refobj, checkfn, changefn}){
+function UserSelect({defaultvalue, usersdata, refobj, checkfn, changefn}){
   return(
-    <select defaultValue="-1" name="selectplayerfour" ref={refobj} onChange={()=>{changefn()}}>
-      <option value="-1" disabled>select player</option>
-      {usersdata.map(user => {
-        let disabled = true;
-        if(checkfn(user.id)){
-          disabled = false;
-        }
-        return <option disabled={disabled} key={user.id} value={user.id}>{user.info.firstname} {user.info.lastname}</option>
-      })}
+    <select
+      value={defaultvalue}
+      name="selectplayerfour"
+      ref={refobj}
+      onChange={()=>{changefn()}}
+    >
+      { usersdata.map(user => {
+        let disabled = user.id !== defaultvalue && checkfn(user.id);
+        return(
+          <option
+            disabled={disabled}
+            key={user.id}
+            value={user.id}
+          >
+            {user.info.firstname} {user.info.lastname}
+          </option>
+        )
+      }) }
     </select>
   )
 }
