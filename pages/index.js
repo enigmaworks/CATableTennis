28b63404b76723877b0e55798b37b62dba8e19ce
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-11-09 21:32:13 -0500

    fix indentation

--- pages/index.js
+++ pages/index.js
@@ -36,40 +36,40 @@ export default function Home(props){
   if(rankingAlgorithm === "winpercent") users = rankByWinPercent(users);
 
   return (<>
-  <Head>
-    <title>Caravel Table Tennis</title>
-  </Head>
-  <header>
-    <h1>Caravel Academy Table Tennis Club</h1>
-  </header>
-  <section>
-    <h2>Leaderboard</h2>
-    <ol className={styles.leaderboard}>
-      <li className={styles.headerrow}>
-        <ul>
-          <li className={styles.rank}><h3>Rank</h3></li>
-          <li><h3>Player</h3></li>
-          <li className={styles.stat}><h3>Elo</h3></li>
-          <li className={styles.stat}><h3>Win %</h3></li>
-          <li className={styles.stat}><h3>Wins</h3></li>
-          <li className={styles.stat}><h3>Losses</h3></li>
-        </ul>
-      </li>
-      {users.map((user, i) => {
-        return (
-        <li className={styles.player} key={i}>
-          <ul className={styles.playerstats}>
-            <li className={styles.rank}>{i+1}</li>
-            <li><h4 className={styles.name}>{user.info.firstname} {user.info.lastname}</h4></li>
-            <li className={styles.stat}>{Math.round(user.elo*100)/100}</li>
-            <li className={styles.stat}>{Math.round(user.winpercent*10000)/100}%</li>
-            <li className={styles.stat}>{user.statistics.w}</li>
-            <li className={styles.stat}>{user.statistics.l}</li>
+    <Head>
+      <title>Caravel Table Tennis</title>
+    </Head>
+    <header>
+      <h1>Caravel Academy Table Tennis Club</h1>
+    </header>
+    <section>
+      <h2>Leaderboard</h2>
+      <ol className={styles.leaderboard}>
+        <li className={styles.headerrow}>
+          <ul>
+            <li className={styles.rank}><h3>Rank</h3></li>
+            <li><h3>Player</h3></li>
+            <li className={styles.stat}><h3>Elo</h3></li>
+            <li className={styles.stat}><h3>Win %</h3></li>
+            <li className={styles.stat}><h3>Wins</h3></li>
+            <li className={styles.stat}><h3>Losses</h3></li>
           </ul>
         </li>
-        );
-      })}
-    </ol>
-  </section>
+        {users.map((user, i) => {
+          return (
+          <li className={styles.player} key={i}>
+            <ul className={styles.playerstats}>
+              <li className={styles.rank}>{i+1}</li>
+              <li><h4 className={styles.name}>{user.info.firstname} {user.info.lastname}</h4></li>
+              <li className={styles.stat}>{Math.round(user.elo*100)/100}</li>
+              <li className={styles.stat}>{Math.round(user.winpercent*10000)/100}%</li>
+              <li className={styles.stat}>{user.statistics.w}</li>
+              <li className={styles.stat}>{user.statistics.l}</li>
+            </ul>
+          </li>
+          );
+        })}
+      </ol>
+    </section>
   </>);
 }
