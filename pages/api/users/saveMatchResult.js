e67e494edf92cda324e8f6743c34d52be5d5579c
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-11-27 09:46:33 -0500

    create api route for saving match results

---
+++ pages/api/users/saveMatchResult.js
@@ -0,0 +1,24 @@
+import {withSessionRoute} from "helpers/lib/config/withSession";
+import users from "helpers/users-data";
+
+export default withSessionRoute(saveMatchResult);
+
+function saveMatchResult(req, res){
+  if(req.method = "POST"){
+    const {results} = req.body;
+    if(req.session && req.session.user){
+      try{
+        for(let i = 0; i < results.length; i++){
+          users.saveMatchResult(results[i].id, results[i].won);
+        }
+        return res.status(200).send();
+      } catch {
+        return res.status(500).send();
+      }
+    } else {
+      return res.status(401).send();
+    }
+  } else {
+    return res.status(405).send();
+  }
+}
