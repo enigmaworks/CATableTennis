e09baf67e7c660dc5daa27bc41074aab8834fb97
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-11-28 11:44:47 -0500

    cleaner user display

--- components/UserDisplay.js
+++ components/UserDisplay.js
@@ -1,8 +1,22 @@
 import Link from "next/link";
+import Router from "next/router";
 import styles from "styles/components/userDisplay.module.css"
 
 export default function UserDisplay(pageprops){
-  
+  if(pageprops.signedin){
+    return (
+      <div className={styles.container}>
+        <Link href="/profile" className={styles.profile}>{pageprops.user.firstname} {pageprops.user.lastname}</Link>
+        <button className="fitcontentwidth light" onClick={signout}>Sign Out</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <button className="fitcontentwidth light" onClick={()=>{Router.push("/login")}}>Sign In</button>
+      </div>
+    );
+  }
 }
 
 function signout(){
