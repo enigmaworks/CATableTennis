c036e73b350474995ea6b4dbfce7f2a9e8c6e41e
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-11-28 08:35:00 -0500

    separate usedisplay styles from nav

--- components/UserDisplay.js
+++ components/UserDisplay.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "styles/components/nav.module.css"
+import styles from "styles/components/userDisplay.module.css"
 
 export default function UserDisplay(pageprops){
   return (
