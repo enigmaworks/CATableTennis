66f74db347ed811faf553a2a6417aa319a4f5932
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-11-28 10:24:14 -0500

    delete old layout

--- components/UserDisplay.js
+++ components/UserDisplay.js
@@ -2,19 +2,7 @@ import Link from "next/link";
 import styles from "styles/components/userDisplay.module.css"
 
 export default function UserDisplay(pageprops){
-  return (
-    <div className={styles.loginout}>
-      {(pageprops.signedin) ? <div>{pageprops.user.username}{" | "}</div> : ""}
-      <div>
-        <Link
-          href={(pageprops.signedin) ? "/" : "/login"}
-          onClick={(pageprops.signedin) ? signout: ()=>{}}
-        >
-          {(pageprops.signedin) ? "Log Out" : "Log In"}
-        </Link>
-      </div>
-    </div>
-  )
+  
 }
 
 function signout(){
