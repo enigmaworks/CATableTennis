239de14a42bc30dbd8bc46bdf7ba4c23fd9713fc
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-11-30 10:55:40 -0500

    input style changes

--- helpers/react-select-theme.js
+++ helpers/react-select-theme.js
@@ -4,7 +4,10 @@ export default function (basetheme) {
       colors: {
         ...basetheme.colors,
         primary: '#691616',
-        primary25: '#40404040',
-      }
+        primary25:'#e8e8e8',
+        primary50:'#40404044',
+        neutral20:'#40404088',
+        neutral30: "#404040"
+      },
   }
 }
