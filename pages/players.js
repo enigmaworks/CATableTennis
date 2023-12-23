fbb6b8f1fc9293048b6fec07cd793341506a2b74
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2023-12-23 10:51:44 -0500

    begin page design

--- pages/players.js
+++ pages/players.js
@@ -4,19 +4,26 @@ import Head from 'next/head';
 export const getServerSideProps = withSessionSsr(
   async ({req, res}) => {
     const user = req.session.user;
+
+    let data = await fetch(process.env.URL + "/api/users/getdata", req);
+    data = await data.json();
+
     if(user){
-      return {props: { signedin: true, user: user }}
+      return {props: { signedin: true, user: user, usersdata: data }}
     } else {
-      return {props: { signedin: false, user: null }}
+      return {props: { signedin: false, user: null, usersdata: data }}
     }
   }
 );
 
-export default function Players(){
+export default function Players(props){
   return (<>
   <Head>
     <title>Players | Caravel Table Tennis</title>
   </Head>
-  Players Page
+  <section>
+    <h2>Current Players</h2>
+    <h2>Graduated Players</h2>
+  </section>
   </>);
 }
