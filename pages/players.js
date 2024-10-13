273e70a2abdfae3ae632502c58af1f29b8798e16
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2024-10-12 20:32:01 -0400

    style changes

--- pages/players.js
+++ pages/players.js
@@ -40,41 +40,39 @@ export default function Players(props){
     <h1>Players</h1>
   </header>
   <section>
-      <ul>
+      <ul className={styles.playerslist}>
         {currentPlayers.map((player, i)=>{
           return (
-            <li id={i}>
-              <ul>
-                <li>{player.info.firstname}</li>
-                <li>{player.info.lastname}</li>
-                <li>{player.info.flair}</li>
-                <li>{player.info.gradyear}</li>
-                <li>{player.statistics.w}</li>
-                <li>{player.statistics.l}</li>
+            <li id={i} className={styles.playercontainer}>
+              <ul className={styles.player}>
+                <li>
+                  {player.info.firstname} {player.info.lastname}
+                  <span className={styles.gradyear}>'{player.info.gradyear.substring(2)}</span>
+                  <span className={styles.flair}>{player.info.flair === ''? '' : player.info.flair}</span>
+                </li>
+                <li className={styles.record}>{player.statistics.w} - {player.statistics.l}</li>
               </ul>
             </li>
           );
         })}
       </ul>
-      <header>
-        <h2>Graduates</h2>
-      </header>
-      <ul>
-          {graduatedPlayers.map((player, i)=>{
+      <h2 className={styles.alumheader}>Alum</h2>
+      <ul className={styles.playerslist}>
+        {graduatedPlayers.map((player, i)=>{
             return (
-              <li id={i}>
-                <ul>
-                  <li>{player.info.firstname}</li>
-                  <li>{player.info.lastname}</li>
-                  <li>{player.info.flair}</li>
-                  <li>{player.info.gradyear}</li>
-                  <li>{player.statistics.w}</li>
-                  <li>{player.statistics.l}</li>
-                </ul>
-              </li>
+              <li id={i} className={styles.playercontainer}>
+              <ul className={styles.player}>
+                <li>
+                  {player.info.firstname} {player.info.lastname}
+                  <span className={styles.gradyear}>'{player.info.gradyear.substring(2)}</span>
+                  <span className={styles.flair}>{player.info.flair === ''? '' : player.info.flair}</span>
+                </li>
+                <li className={styles.record}>{player.statistics.w} - {player.statistics.l}</li>
+              </ul>
+            </li>
             );
           })}
-        </ul>
+      </ul>
   </section>
   </>);
 }
