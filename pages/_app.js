62dc57e48791d1275628a2b8aa18b2f72e861e4a
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2024-10-18 22:39:40 -0400

    fix FOUC

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "/styles/globals.css";
 import Nav from "/components/Nav";
 import UserDisplay from "/components/UserDisplay";
+import useTheme from "/helpers/useTheme";
 
 export default function App({ Component, pageProps }) {
+  useTheme();
+
   return( <>
+    <style>
+      {'body:not(.body-visible){visibility: hidden;}'}
+    </style>
     <Nav {...pageProps}></Nav>
     <UserDisplay {...pageProps} ></UserDisplay>
     <main>
